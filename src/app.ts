5e758fef2c7eef159255de841370d6a1cb313244
Arman <mdarman365506@gmail.com>
2025-03-22 16:16:27 +0600

    working on order functionality

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import { ProductRoutes } from "./app/modules/products/product.routes";
+import {  OrderRoutes } from "./app/modules/orders/order.routes";
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors())
 
 app.use('/api/products', ProductRoutes)
+app.use('/api/orders', OrderRoutes)
 
 app.get("/", (req, res) => {
   res.send("SwiftCommerce Server is running....!");
---
+++ src/app/modules/orders/order.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+
+
+const createOrder = async (req: Request, res: Response) => {
+ try {
+    
+ } catch (error) {
+    
+ }
+   
+};
+
+
+export const OrderController = {
+  createOrder,
+
+};
