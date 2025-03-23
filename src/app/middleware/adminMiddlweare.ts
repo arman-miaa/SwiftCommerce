512945e6cdca681ac94453d85ddcff8b935b5d56
Arman <mdarman365506@gmail.com>
2025-03-24 00:02:56 +0600

    project done

---
+++ src/app/middleware/adminMiddlweare.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+
+
+
+
+export const isAdmin = (req: Request, res: Response, nex: NextFunction) => {
+    const userRole = (req as any).decoded.role;
+    if (userRole !== 'admin') {
+        res.status(403).json({ success: false, message: 'You are not authorized to perform this action' })
+        return;
+    }
+    nex();
+}
--- src/app/modules/products/product.routes.ts
+++ src/app/modules/products/product.routes.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { ProductControllers } from "./product.controller";
 import { verifyToken } from "../../middleware/authMiddleware";
+import { isAdmin } from "../../middleware/adminMiddlweare";
 const router = express.Router();
 
 
-router.get("/",ProductControllers.getAllProducts)
-router.post("/", ProductControllers.createProduct)
-router.get("/:productId",verifyToken, ProductControllers.getSingleProduct)
-router.put("/:productId", ProductControllers.updateProduct)
-router.delete("/:productId", ProductControllers.deleteProduct)
+router.get("/", verifyToken,isAdmin,ProductControllers.getAllProducts)
+router.post("/", verifyToken,isAdmin, ProductControllers.createProduct)
+router.get("/:productId",verifyToken, isAdmin, ProductControllers.getSingleProduct)
+router.put("/:productId", verifyToken,isAdmin, ProductControllers.updateProduct)
+router.delete("/:productId", verifyToken, isAdmin, ProductControllers.deleteProduct)
 
 
 export const ProductRoutes = router;
