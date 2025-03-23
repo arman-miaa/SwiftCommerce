9368c0c18df3cbd6fc3449c9a227568272e26d59
Arman <mdarman365506@gmail.com>
2025-03-24 00:42:00 +0600

    removed token from github

--- src/app/middleware/authMiddleware.ts
+++ src/app/middleware/authMiddleware.ts
@@ -7,7 +7,7 @@ const JWT_SECRET = config.jwt_secret as string;
 
 
 export const verifyToken = (req: Request, res: Response, nex: NextFunction) => {
-  // will get token this formate: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMUBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4xIiwiaWF0IjoxNzQyNzQ4NjQ0LCJleHAiOjE3NDI3NjY2NDR9.ccyp9LPYiNr8cX-V15oTyYv38a_ek7tcILy1Esr0Npo
+  // will get token this formate: Bearer .....
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
         res.status(401).send({ message: "Invalid token and Access Denied" });
